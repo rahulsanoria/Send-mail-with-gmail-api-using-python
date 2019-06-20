9554785a3bb279e4fe89fc51902c55c5053a1fb2
Rahul Sanoria <39688195+rahulsanoria@users.noreply.github.com>
2019-06-20 10:12:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+import auth
+def get_labels():
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+        print('Labels:')
+        for label in labels:
+            print(label['name'])
+
+SCOPES = 'https://mail.google.com/'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
+authInst = auth.auth(SCOPES,CLIENT_SECRET_FILE,APPLICATION_NAME)
+credentials = authInst.get_credentials()
+
+http = credentials.authorize(httplib2.Http())
+service = discovery.build('gmail', 'v1', http=http)
+
+import send_mail
+
+sendInst = send_mail.send_mail(service)
+message = sendInst.create_message_with_attachment('rahul.kumar25011999@gmail.com','rahul.kumar25011999@gmail.com','Testing 123','Hi there, This is a test from Python!','image.jpg')
+sendInst.send_message('me',message)
